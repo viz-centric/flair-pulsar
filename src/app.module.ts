be825a31420165309d661723b643b40c6c10e45f
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2019-07-27 18:40:50 +0300

    removed boilerplace app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
-import {AppService} from './app.service';
 import {ConfigService} from './config/config.service';
-import {RpcClientService} from "./rpc/rpcclient/rpc-client.service";
 import {RpcConfigService} from "./rpc/rpcconfig/rpc-config.service";
 import {RpcServerService} from "./rpc/server/rpc-server.service";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, ConfigService, RpcServerService, RpcClientService, RpcConfigService],
+  providers: [ConfigService, RpcServerService, RpcConfigService],
 })
 export class AppModule {}
